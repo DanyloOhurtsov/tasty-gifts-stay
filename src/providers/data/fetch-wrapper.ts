37b2e0050d9a0954460834476d99836c7e42473b
DanyloOhurtsov <d.ohurtsov.fed@gmail.com>
2024-01-30 13:44:14 +0100

    Init commit

---
+++ src/providers/data/fetch-wrapper.ts
@@ -0,0 +1,61 @@
+import { GraphQLFormattedError } from "graphql";
+
+type Error = {
+    message: string;
+    statusCode: string;
+};
+
+const custonFetch = async (url: string, options: RequestInit) => {
+    const accessToken = localStorage.getItem("access_token");
+
+    const headers = options.headers as Record<string, string>;
+    return await fetch(url, {
+        ...options,
+        headers: {
+            ...headers,
+            Authorization: headers?.Authorization || `Bearer ${accessToken}`,
+            "Content-Type": "application/json",
+            "Apollo-Require-Preflight": "true",
+        },
+    });
+};
+
+const getGraphQLErrors = (
+    body: Record<"errors", GraphQLFormattedError[] | undefined>
+): Error | null => {
+    if (!body) {
+        return {
+            message: "Unknown record",
+            statusCode: "INTERNAL_SERVER_ERROR",
+        };
+    }
+
+    if ("errors" in body) {
+        const errors = body?.errors;
+
+        const messeges = errors?.map((error) => error?.message)?.join("");
+        const code = errors?.[0]?.extensions?.code;
+
+        return {
+            message: messeges || JSON.stringify(errors),
+            statusCode: code || 500,
+        };
+    }
+
+    return null;
+};
+
+export const fetchWrapper = async (url: string, options: RequestInit) => {
+    const response = await custonFetch(url, options);
+
+    const responseClone = response.clone();
+    const body = await responseClone.json();
+
+    const error = getGraphQLErrors(body);
+
+    if (error) {
+        throw error;
+    }
+
+    return response;
+};
