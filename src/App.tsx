15e2d79c6d5d4fbce983d8970af3e04a478c580b
DanyloOhurtsov <d.ohurtsov.fed@gmail.com>
2024-01-30 13:56:34 +0100

    Created liveProvider and dataProvider

--- src/App.tsx
+++ src/App.tsx
@@ -5,53 +5,49 @@ import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 import { useNotificationProvider } from "@refinedev/antd";
 import "@refinedev/antd/dist/reset.css";
 
-import dataProvider, {
-  GraphQLClient,
-  liveProvider,
-} from "@refinedev/nestjs-query";
+import { dataProvider, liveProvider } from "./providers";
 import routerBindings, {
-  DocumentTitleHandler,
-  UnsavedChangesNotifier,
+    DocumentTitleHandler,
+    UnsavedChangesNotifier,
 } from "@refinedev/react-router-v6";
 import { App as AntdApp } from "antd";
 import { createClient } from "graphql-ws";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-
 function App() {
-  return (
-    <BrowserRouter>
-      <GitHubBanner />
-      <RefineKbarProvider>
-          <AntdApp>
-            <DevtoolsProvider>
-              <Refine
-                // dataProvider={dataProvider(gqlClient)}
-                // liveProvider={liveProvider(wsClient)}
-                notificationProvider={useNotificationProvider}
-                routerProvider={routerBindings}
-                // authProvider={authProvider}
-                options={{
-                  syncWithLocation: true,
-                  warnWhenUnsavedChanges: true,
-                  useNewQueryKeys: true,
-                  projectId: "4E7N0J-5mn8GO-E8tFxp",
-                  liveMode: "auto",
-                }}
-              >
-                <Routes>
-                  <Route index element={<WelcomePage />} />
-                </Routes>
-                <RefineKbar />
-                <UnsavedChangesNotifier />
-                <DocumentTitleHandler />
-              </Refine>
-              <DevtoolsPanel />
-            </DevtoolsProvider>
-          </AntdApp>
-      </RefineKbarProvider>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <GitHubBanner />
+            <RefineKbarProvider>
+                <AntdApp>
+                    <DevtoolsProvider>
+                        <Refine
+                            dataProvider={dataProvider}
+                            liveProvider={liveProvider}
+                            notificationProvider={useNotificationProvider}
+                            routerProvider={routerBindings}
+                            // authProvider={authProvider}
+                            options={{
+                                syncWithLocation: true,
+                                warnWhenUnsavedChanges: true,
+                                useNewQueryKeys: true,
+                                projectId: "4E7N0J-5mn8GO-E8tFxp",
+                                liveMode: "auto",
+                            }}
+                        >
+                            <Routes>
+                                <Route index element={<WelcomePage />} />
+                            </Routes>
+                            <RefineKbar />
+                            <UnsavedChangesNotifier />
+                            <DocumentTitleHandler />
+                        </Refine>
+                        <DevtoolsPanel />
+                    </DevtoolsProvider>
+                </AntdApp>
+            </RefineKbarProvider>
+        </BrowserRouter>
+    );
 }
 
 export default App;
