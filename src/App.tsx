6fcdda65d78b4fef40a4c9925ba2085c46d8606d
Danylo Ohurtsov <d.ohurtsov.fed@gmail.com>
2024-01-31 16:10:00 +0100

    New dependencies installed
    Auth component routing

--- src/App.tsx
+++ src/App.tsx
@@ -6,48 +6,54 @@ import { useNotificationProvider } from "@refinedev/antd";
 import "@refinedev/antd/dist/reset.css";
 
 import { authProvider, dataProvider, liveProvider } from "./providers";
+import { Home, ForgotPassword, Login, Register } from "./pages";
+
 import routerBindings, {
-    DocumentTitleHandler,
-    UnsavedChangesNotifier,
+  DocumentTitleHandler,
+  UnsavedChangesNotifier,
 } from "@refinedev/react-router-v6";
 import { App as AntdApp } from "antd";
 import { createClient } from "graphql-ws";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <GitHubBanner />
-            <RefineKbarProvider>
-                <AntdApp>
-                    <DevtoolsProvider>
-                        <Refine
-                            dataProvider={dataProvider}
-                            liveProvider={liveProvider}
-                            notificationProvider={useNotificationProvider}
-                            routerProvider={routerBindings}
-                            authProvider={authProvider}
-                            options={{
-                                syncWithLocation: true,
-                                warnWhenUnsavedChanges: true,
-                                useNewQueryKeys: true,
-                                projectId: "4E7N0J-5mn8GO-E8tFxp",
-                                liveMode: "auto",
-                            }}
-                        >
-                            <Routes>
-                                <Route index element={<WelcomePage />} />
-                            </Routes>
-                            <RefineKbar />
-                            <UnsavedChangesNotifier />
-                            <DocumentTitleHandler />
-                        </Refine>
-                        <DevtoolsPanel />
-                    </DevtoolsProvider>
-                </AntdApp>
-            </RefineKbarProvider>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <GitHubBanner />
+      <RefineKbarProvider>
+        <AntdApp>
+          <DevtoolsProvider>
+            <Refine
+              dataProvider={dataProvider}
+              liveProvider={liveProvider}
+              notificationProvider={useNotificationProvider}
+              routerProvider={routerBindings}
+              authProvider={authProvider}
+              options={{
+                syncWithLocation: true,
+                warnWhenUnsavedChanges: true,
+                useNewQueryKeys: true,
+                projectId: "4E7N0J-5mn8GO-E8tFxp",
+                liveMode: "auto",
+              }}
+            >
+              <Routes>
+                <Route index element={<WelcomePage />} />
+                <Route index element={<Home />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/forgot-password" element={<ForgotPassword />} />
+              </Routes>
+              <RefineKbar />
+              <UnsavedChangesNotifier />
+              <DocumentTitleHandler />
+            </Refine>
+            <DevtoolsPanel />
+          </DevtoolsProvider>
+        </AntdApp>
+      </RefineKbarProvider>
+    </BrowserRouter>
+  );
 }
 
 export default App;
