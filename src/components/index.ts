d67edb86058f702a3f7e06388e7c51a13255ee8f
Danylo Ohurtsov <d.ohurtsov.fed@gmail.com>
2024-02-02 19:01:08 +0100

    Home page creating proccess (Minor changes)

---
+++ src/components/index.ts
@@ -0,0 +1,17 @@
+import UpcomingEvents from "./home/UpcomingEvents";
+import DealsChart from "./home/DealsChart";
+import UpcomingEventsSkeleton from "./skeleton/upcoming-events";
+import AccordionHeaderSkeleton from "./skeleton/accordion-header";
+import KanbanColumnSkeleton from "./skeleton/kanban";
+import LatestActivitiesSkeleton from "./skeleton/latest-activities";
+import ProjectCardSkeleton from "./skeleton/project-card";
+
+export {
+    UpcomingEvents,
+    DealsChart,
+    UpcomingEventsSkeleton,
+    AccordionHeaderSkeleton,
+    KanbanColumnSkeleton,
+    LatestActivitiesSkeleton,
+    ProjectCardSkeleton,
+};
