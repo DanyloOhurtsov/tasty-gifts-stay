e99a0d522489c16bc3841f59f695325270be2afa
DanyloOhurtsov <d.ohurtsov.fed@gmail.com>
2024-02-02 14:09:53 +0100

    Account setting components created

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,13 +1,26 @@
-import CurrentUser from "./CurrentUser"
+import { Layout, Space } from "antd";
+import CurrentUser from "./CurrentUser";
 
 const Header = () => {
-  return (
-    <div>
-        <CurrentUser/>
-    </div>
-  )
-}
+    
+    const headerStyles: React.CSSProperties = {
+        background: "#fff",
+        display: "flex",
+        justifyContent: "flex-end",
+        alignItems: "center",
+        padding: "0 24px",
+        position: "sticky",
+        top: 0,
+        zIndex: 999,
+    };
 
+    return (
+        <Layout.Header style={headerStyles}>
+            <Space size={"middle"} align="center">
+                <CurrentUser />
+            </Space>
+        </Layout.Header>
+    );
+};
 
-
-export default Header
+export default Header;
