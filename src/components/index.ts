5ad0dc8857a55a8e69058406fa92ccbcdccbf9e3
DanyloOhurtsov <d.ohurtsov.fed@gmail.com>
2024-02-05 13:14:04 +0100

    DashboardTotalCountCard components ready

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,13 +5,17 @@ import AccordionHeaderSkeleton from "./skeleton/accordion-header";
 import KanbanColumnSkeleton from "./skeleton/kanban";
 import LatestActivitiesSkeleton from "./skeleton/latest-activities";
 import ProjectCardSkeleton from "./skeleton/project-card";
+import DashboardTotalCountCard from "./home/DashboardTotalCountCard";
 
 export {
     UpcomingEvents,
     DealsChart,
+    
     UpcomingEventsSkeleton,
     AccordionHeaderSkeleton,
     KanbanColumnSkeleton,
     LatestActivitiesSkeleton,
     ProjectCardSkeleton,
+
+    DashboardTotalCountCard,
 };
