cce4a94ef9afe087b36afb0c590458427034430d
DanyloOhurtsov <d.ohurtsov.fed@gmail.com>
2024-02-05 11:52:40 +0100

    UpcomingEvents components ready

--- src/components/home/UpcomingEvents.tsx
+++ src/components/home/UpcomingEvents.tsx
@@ -1,6 +1,5 @@
 import { CalendarOutlined } from "@ant-design/icons";
 import { Badge, Card, List } from "antd";
-import React, { useState } from "react";
 import { Text } from "../text";
 import UpcomingEventsSkeleton from "../skeleton/upcoming-events";
 import { getDate } from "@/utilities/helpers";
@@ -9,9 +8,7 @@ import { DASHBORAD_CALENDAR_UPCOMING_EVENTS_QUERY } from "@/graphql/queries";
 import dayjs from "dayjs";
 
 const UpcomingEvents = () => {
-    const [isLoading, setIsLoading] = useState(false);
-
-    const { data, isLoading: eventsLoading } = useList({
+    const { data, isLoading } = useList({
         resource: "events",
         pagination: { pageSize: 5 },
         sorters: [
@@ -87,20 +84,19 @@ const UpcomingEvents = () => {
                             </List.Item>
                         );
                     }}
-                    {!isLoading && data?.data.length === 0 && (
-                        <span
-                            style={{
-                                display: 'flex'
-                                just
-                            }}
-                        >
-                            No upcoming events
-                        </span>
-
-                    )}
+                ></List>
+            )}
+            {!isLoading && data?.data.length === 0 && (
+                <span
+                    style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        height: "220px",
+                    }}
                 >
-
-                </List>
+                    No upcoming events
+                </span>
             )}
         </Card>
     );
